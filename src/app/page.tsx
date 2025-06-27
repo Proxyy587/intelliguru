5299f99ba598c15c30d760597afa80712c6b510e
Abhijit <abhijitbhattacharjee333@gmail.com>
2025-06-27 15:17:33 +0530

    file structure setup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <>
-      <div className="flex w-full items-center justify-center">
-        
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex w-full h-screen items-center justify-center">
+				<div className="flex flex-col items-center justify-center">
+					<h1 className="text-4xl font-bold">Intelliguru </h1>
+					<p className="text-lg">
+						Transforming the way you learn with AI-powered education
+					</p>
+					<Link href="/login">
+						<Button className="mt-4">Get Started</Button>
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 }
